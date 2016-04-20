6810809705f0bebde3f071cfffee17e954d7949a
Max Baluev <Max Baluev>
2016-04-20 20:31:58 +0500

    preview

---
+++ app.js
@@ -0,0 +1,148 @@
+
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcrypt-nodejs');
+
+//Создаем новую бд и коллекцию в памяти при каждом запуске ноды
+var loki = require('lokijs');
+var db = new loki("loki.db");
+var users = db.addCollection('users');
+
+//Заполняем базу пустыми данными(нужно для генерации мест на клиенте)
+var seats = db.addCollection('seats');
+for (var i = 1; i < 26; i++) {
+   seats.insert({
+        pos: i,
+        name: false,
+        color: '#fff' 
+      });       
+}
+
+
+//Секретный ключ для подписи токена
+var secret = 'super_secret_key';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+server.listen(8080, function () {
+  console.log('Подняли сервер на *:8080');
+});
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+        
+app.get('/style.css', function (req, res) {
+  res.sendFile(__dirname + '/style.css');
+});
+
+app.get('/index.js', function (req, res) {
+  res.sendFile(__dirname + '/index.js');
+});
+
+//Безопасно только при использовании защищенного соединения
+app.post('/api/users', function (req, res, next) {  
+  
+  var username = req.body.username;
+  var password = req.body.password;
+  //bcrypt sync password hash
+  console.log(1);
+  var hash = bcrypt.hashSync(password);
+  console.log(2);
+  //Проверяем существует ли пользователь в бд
+  var user = users.findOne({'name':  username});  
+  
+   //Если нет в бд - заносим в базу вместе с цветом
+   if(!user){
+      var rnd_color = '#' + Math.floor(Math.random()*16777215).toString(16)
+      users.insert({
+        name: username,
+        hash: hash, //todo hash pass
+        color: rnd_color //рандомный цвет
+      });       
+      console.log(hash);
+    }else if(user.name === username){
+      //Todo отслеживание неправильных попыток, и блокировака по ip
+      // на некоторое время для защиты от брутфорса
+      
+      //Чекаем хеш
+      var chk = bcrypt.compareSync(password, user.hash);
+      if(!chk){
+        //Не удалось авторизоваться, отдаем http статус 401   
+        res.sendStatus(401); 
+        return;       
+      }   
+    }
+    
+    //генерируем токен для пользователя
+    var user_data = {
+      username: username,
+      color: rnd_color || user.color
+    };          
+    var token = jwt.sign(user_data, secret, {expiresIn: 30*60}); 
+    
+    res.json({token: token});  
+});
+
+
+//Проверка валидности токена
+io.use(
+  require('socketio-jwt').authorize({
+  secret: secret,
+  handshake: true
+}));
+
+io.on('connection', function (socket) {
+    //Отправляем данные для генерации таблицы мест
+    socket.emit('start', seats.data);
+    socket.on('reserve', function (data) {
+      
+      //получаем количество забронированных мест у пользователя
+      var user_seats = seats.find({'name':{'$eq': socket.decoded_token.username}}).length;
+      
+      //проверяем можно ли зарезервировать место
+      var seat = seats.findOne({'pos': { '$eq' :  parseInt(data.id)}});
+      //Проверяем статус места
+      if(seat.name === false && user_seats < 5){
+        //место свободно, занимаем его и отправляем инфу всем клиентам
+        seat.name = socket.decoded_token.username;
+        seat.color = socket.decoded_token.color;
+        seats.update(seat);
+        io.sockets.emit('seat taken',{
+          pos: data.id,
+          color: socket.decoded_token.color,
+          name: socket.decoded_token.username
+        });
+        return;   
+      }else if(seat.name === socket.decoded_token.username){
+        //Снимаем бронь
+        seat.name = false;    
+        seat.color = "#fff"; 
+        seats.update(seat);
+        io.sockets.emit('seat taken',{
+          pos: data.id,
+          color: "#fff",
+          name: false
+        });  
+        return;
+      }else{
+        //место занято        
+      }
+      
+      if(user_seats == 5){
+        //Юзер занял слишком много мест
+        socket.emit('maximum seats');
+        return;
+      }      
+      
+    });    
+
+    console.log('В базе пользователей:', users.count());    
+});
---
+++ index.js
@@ -0,0 +1,109 @@
+ $(document).ready(function () {
+      var socket,token;
+      $('#login-modal').modal('show');
+      $('#login').prop('disabled', false);
+        
+      $('#login').on("click", function(e) {
+        var username = $('input[name=username]').val();
+        var password = $('input[name=password]').val();
+        if (!username) {
+          alert('Вы не ввели логин');
+          return;
+        }
+        if (!password) {
+          alert('Вы не ввели пароль');
+          return;
+        }
+        
+        var user = {
+          username: username,
+          password: password
+        };
+        
+        //Отправляем запрос с телефоном на сервер, для получения токена
+        $.ajax({
+          type: 'POST',
+          url: 'http://localhost:8080/api/users',
+          data: user
+        }).done(function (data) {
+          token = data.token;
+          if (!token) {
+            alert('Не удалось авторизоваться.');
+            return;
+          }
+          console.log(token);
+          socket = io.connect('http://localhost:8080', {
+            query: 'token=' + token,
+            forceNew: true
+          })
+          
+          socket.on('connect', function (data) {
+            console.log('Подключились к вебсокет серверу');
+            $('#login-modal').modal('toggle');
+            $('.content').css('display','block');
+            socket.on('start', function (data) {
+              //Смотрим каждый json элемент
+              $.each(data, function (key, data) {
+                  console.log(data)
+                  var html = "<div class='seat' id=" + data.pos + "></div>"
+                  $(".squad").append(html);
+                  $('#'+data.pos).css('background-color', data.color).attr("taken", data.name);
+                  if(data.name === false){
+                    $('#'+data.pos).text('');
+                  }else{
+                    $('#'+data.pos).text(data.name);
+                  }
+              })
+              //Настраиваем действия при наведении
+              $(".seat").hover(function(e){
+                  if(($(this).attr("taken") === "false") || ($(this).attr("taken") === username)){
+                    $(this).css('cursor','pointer');
+                  }else{
+                    $(this).css('cursor','default');
+                  }
+              });
+              //Настраиваем действия при клике
+              $(".seat").click(function(e){
+                if(($(this).attr("taken") === "false") || ($(this).attr("taken") === username)){
+                  console.log(this.id);
+                  socket.emit('reserve', { id: this.id });
+                }
+              });
+              //Логаут
+              $("#logout").click(function(e){
+                  socket.disconnect();
+                  $('.content').css('display','none');
+                  $('.squad').html('');
+                  $('#login-modal').modal('show');
+              });
+            });
+            
+            socket.on('seat taken', function(data){
+              $('#'+data.pos).css('background-color', data.color).attr("taken",data.name);
+              if(data.name === false){
+                $('#'+data.pos).text('');
+              }else{
+                $('#'+data.pos).text(data.name);                
+              }
+            });
+            
+            socket.on('maximum seats', function(data){
+              alert('Пользователь может занять максимум 5 мест');
+            })
+          });
+          
+          socket.on('error', function (err) {
+            console.log(JSON.stringify(err));
+          });
+          
+        
+        }).fail(function() {
+          alert("Неправильный пароль");
+        });
+        
+      });
+      
+      
+      
+      
+    });
